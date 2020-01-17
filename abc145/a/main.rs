8df45a05133a5695ba62b74852b672508f606393
atomiyama <tomisuker16@gmail.com>
2020-01-17 12:36:56 +0900

    [AC]abc145a

---
+++ abc145/a/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+  let r = read::<u32>();
+  println!("{}", r*r);
+}
+
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
